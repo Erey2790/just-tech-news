bbf5d37baf4a6d441e02e06cc2eee006a7957a46
ElmerPC <erey2790@gmail.com>
2022-02-22 18:29:46 -0600

    added final dashboard routes js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Vote } = require('../../models');
-const sequelize = require('../../config/connection')
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -51,12 +52,12 @@ router.get('/:id', (req, res) => {
 });
 
 // CREATE A POST
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // EXPECTS {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -66,7 +67,7 @@ router.post('/', (req, res) => {
 });
 // CREATE PUT ROUTE FOR VOTING ON A POST
 // PUT /API/POSTS/UPVOTE
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // make sure the session exist first
     if (req.session) {
         // pass session id along with all destructured properties on req.body
@@ -80,7 +81,7 @@ router.put('/upvote', (req, res) => {
 });
 
 // UPDATE A POST'S TITLE
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -105,7 +106,7 @@ router.put('/:id', (req, res) => {
 })
 
 // DELETE A POST
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { response } = require('express');
-const { User, Post, Vote } = require('../../models')
+const { User, Post, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -49,7 +50,7 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'passwword1234'}
     User.create({
         username: req.body.username,
@@ -68,7 +69,7 @@ router.post('/', (req, res) => {
 });
 
 // CREATE THE LOGIN ROUTE FOR AUTHENTICATION
-router.post('/login', (req, res) => {
+router.post('/login', withAuth, (req, res) => {
     User.findOne({
       where: {
         email: req.body.email
@@ -98,7 +99,7 @@ router.post('/login', (req, res) => {
   });
 
 // Put /api/users/1 update existing data
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com, password: 'password1234'}
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
@@ -122,7 +123,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
@@ -142,7 +143,7 @@ router.delete('/:id', (req, res) => {
 });
 
 // ADD LOGIC TO DESTROY THE SESSION
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
